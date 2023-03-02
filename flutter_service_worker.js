61994433f2f0d1635153e28f4e337ac9b27c758c
Pedro Lemos <lemozxpedro@gmail.com>
2023-03-02 20:49:10 -0300

    Manual build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,16 +14,11 @@ const RESOURCES = {
 "canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "04c1d4d0a001576bf04ba1781c2b36d1",
-"/": "04c1d4d0a001576bf04ba1781c2b36d1",
-"main.dart.js": "8caa5bb13de1de0fc6d1f5172134dea1",
-"manifest.json": "cfda885e6ec07fcddbceb0472e706065",
+"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
+"index.html": "adb18c5b612da8803cb73a54cc677298",
+"/": "adb18c5b612da8803cb73a54cc677298",
+"main.dart.js": "888268632df853ed2bb3c4fc77efd3d8",
+"manifest.json": "1a12000c46b70c593a2854428706523d",
 "version.json": "5a9a97ad502db57ff216be2900df4fd4"
 };
 
