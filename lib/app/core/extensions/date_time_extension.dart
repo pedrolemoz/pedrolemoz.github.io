57ea2d4d19fe2d4527105510bf44078d11ab18f7
Pedro Lemos <lemozxpedro@gmail.com>
2023-03-05 17:47:33 -0300

    Added extension to get month as String

---
+++ lib/app/core/extensions/date_time_extension.dart
@@ -0,0 +1,34 @@
+extension DateTimeExtension on DateTime {
+  String get monthAsString {
+    final month = this.month;
+
+    if (month <= 0 || month > 12) return '';
+
+    switch (month) {
+      case 1:
+        return 'January';
+      case 2:
+        return 'February';
+      case 3:
+        return 'March';
+      case 4:
+        return 'April';
+      case 5:
+        return 'May';
+      case 6:
+        return 'June';
+      case 7:
+        return 'July';
+      case 8:
+        return 'August';
+      case 9:
+        return 'September';
+      case 10:
+        return 'October';
+      case 11:
+        return 'November';
+      default:
+        return 'December';
+    }
+  }
+}
