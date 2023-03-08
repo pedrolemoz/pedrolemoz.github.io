bb045bdf5633fa9b4d50cebb7bc8386263d2c6cc
Pedro Lemos <lemozxpedro@gmail.com>
2023-03-08 09:10:31 -0300

    Centered initial page in mobile view

--- lib/app/modules/initial_module/pages/initial_page.dart
+++ lib/app/modules/initial_module/pages/initial_page.dart
@@ -42,68 +42,70 @@ class InitialPage extends StatelessWidget {
 class _InitialPageContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        CircleAvatar(
-          backgroundImage: NetworkImage(Assets.me),
-          maxRadius: 120,
-          minRadius: 50,
-        ),
-        SizedBox(height: 16),
-        Text(
-          'Pedro Lemos',
-          style: Theme.of(context)
-              .textTheme
-              .displayMedium!
-              .copyWith(fontWeight: FontWeight.bold),
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(height: 8),
-        Text(
-          'Software Engineer',
-          style: Theme.of(context).textTheme.headlineSmall,
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(height: 16),
-        Wrap(
-          alignment: WrapAlignment.center,
-          children: [
-            LabeledButton(
-              text: 'LinkedIn',
-              icon: UniconsLine.linkedin,
-              onTap: () => launchUrl(Links.linkedIn),
-            ),
-            LabeledButton(
-              text: 'GitHub',
-              icon: UniconsLine.github,
-              onTap: () => launchUrl(Links.gitHub),
-            ),
-            LabeledButton(
-              text: 'Email',
-              icon: UniconsLine.envelope,
-              onTap: () => launchUrl(Links.email),
-            ),
-          ],
-        ),
-        SizedBox(height: 8),
-        Wrap(
-          alignment: WrapAlignment.center,
-          children: [
-            LabeledButton(
-              text: 'Resume (EN)',
-              icon: UniconsLine.file_download,
-              onTap: () => launchUrl(Assets.resumeEnglish),
-            ),
-            LabeledButton(
-              text: 'Resume (PT)',
-              icon: UniconsLine.file_download,
-              onTap: () => launchUrl(Assets.resumePortuguese),
-            ),
-          ],
-        ),
-      ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(Assets.me),
+            maxRadius: 120,
+            minRadius: 50,
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Pedro Lemos',
+            style: Theme.of(context)
+                .textTheme
+                .displayMedium!
+                .copyWith(fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Software Engineer',
+            style: Theme.of(context).textTheme.headlineSmall,
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 16),
+          Wrap(
+            alignment: WrapAlignment.center,
+            children: [
+              LabeledButton(
+                text: 'LinkedIn',
+                icon: UniconsLine.linkedin,
+                onTap: () => launchUrl(Links.linkedIn),
+              ),
+              LabeledButton(
+                text: 'GitHub',
+                icon: UniconsLine.github,
+                onTap: () => launchUrl(Links.gitHub),
+              ),
+              LabeledButton(
+                text: 'Email',
+                icon: UniconsLine.envelope,
+                onTap: () => launchUrl(Links.email),
+              ),
+            ],
+          ),
+          SizedBox(height: 8),
+          Wrap(
+            alignment: WrapAlignment.center,
+            children: [
+              LabeledButton(
+                text: 'Resume (EN)',
+                icon: UniconsLine.file_download,
+                onTap: () => launchUrl(Assets.resumeEnglish),
+              ),
+              LabeledButton(
+                text: 'Resume (PT)',
+                icon: UniconsLine.file_download,
+                onTap: () => launchUrl(Assets.resumePortuguese),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
