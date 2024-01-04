74505fb382da9c93cb373703c5b7768fcf6cdf17
Pedro Lemos <lemozxpedro@gmail.com>
2024-01-04 03:17:40 -0300

    [WASM] Automatic build (2024-01-04 03:17:40)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,11 +30,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "d0e796e8394df366a6b23c9fecaece11
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a87d2d1d0342184d4c06ebca751569d6",
-"/": "a87d2d1d0342184d4c06ebca751569d6",
+"index.html": "c6cc834f493f52e32f5306f35df3c359",
+"/": "c6cc834f493f52e32f5306f35df3c359",
 "main.dart.js": "b1077ac802b7f547c647fb8c31ede050",
 "main.dart.mjs": "5f24ecb8f8dca4033c6c9ebc152760df",
-"main.dart.wasm": "c5269eb98bba2358a1c91b415911dc1d",
+"main.dart.wasm": "fb006b654d7c7bdc59eeb1fed67a812f",
 "manifest.json": "84f05298312f6aa09d906a96163131c8",
 "version.json": "5a9a97ad502db57ff216be2900df4fd4"};
 // The application shell files that are downloaded before a service worker can
