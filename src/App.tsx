d88fef798af1d351e15083b947eb7eeeefedaad9
Pedro Lemos <lemozxpedro@gmail.com>
2025-04-18 04:01:39 -0300

    refactor: adjusting routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 import PolicyPage from "./PolicyPage";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirectPath = params.get("p");
+    if (redirectPath) {
+      window.history.replaceState({}, "", redirectPath);
+    }
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
